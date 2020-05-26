a0958be1aeeb68da92c606bc78593d91a1735996
Usama <mohamadusama242@gmail.com>
2020-05-26 17:48:32 +0200

    Basic cleanup

--- js/app.js
+++ js/app.js
@@ -1,18 +1,3 @@
-/**
- *
- * Manipulating the DOM exercise.
- * Exercise programmatically builds navigation,
- * scrolls to anchors from navigation,
- * and highlights section in viewport upon scrolling.
- *
- * Dependencies: None
- *
- * JS Version: ES2015/ES6
- *
- * JS Standard: ESlint
- *
- */
-
 const sections = document.querySelectorAll("section");
 const navbar = document.getElementById("navbar__list");
 
